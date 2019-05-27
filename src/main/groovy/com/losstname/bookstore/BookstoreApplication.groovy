b5532a9cbc6306892dab19887b5f08a2ea78b92d
losstname <losstname@gmail.com>
2019-05-28 04:53:39 +0800

    Initial commit

---
+++ src/main/groovy/com/losstname/bookstore/BookstoreApplication.groovy
@@ -0,0 +1,13 @@
+package com.losstname.bookstore
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BookstoreApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication, args)
+	}
+
+}

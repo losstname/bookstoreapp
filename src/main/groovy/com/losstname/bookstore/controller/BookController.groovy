c07609e8f8abdcc94f524808388c7e4c08b33b18
losstname <losstname@gmail.com>
2019-05-29 23:10:55 +0800

    Add Book and Customer controller

---
+++ src/main/groovy/com/losstname/bookstore/controller/BookController.groovy
@@ -0,0 +1,14 @@
+package com.losstname.bookstore.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+/**
+ * Created by umarwhk(umrwhk@gmail.com) on 2019-05-29.
+ */
+@Controller
+@RequestMapping("/book")
+class BookController {
+
+
+}
---
+++ src/main/groovy/com/losstname/bookstore/controller/CustomerController.groovy
@@ -0,0 +1,13 @@
+package com.losstname.bookstore.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+/**
+ * Created by umarwhk(umrwhk@gmail.com) on 2019-05-29.
+ */
+@Controller
+@RequestMapping("/customer")
+class CustomerController {
+
+}
